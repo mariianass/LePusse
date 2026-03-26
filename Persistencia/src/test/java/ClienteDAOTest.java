e989e526c7da5abbca48e03d4341f742ed7e02a3
mariianass <omariana1073@gmail.com>
2026-03-25 21:52:43 -0700

    fix: error prueba

--- Persistencia/src/test/java/ClienteDAOTest.java
+++ Persistencia/src/test/java/ClienteDAOTest.java
@@ -35,25 +35,5 @@ public class ClienteDAOTest {
 //        assertEquals("Regina", resultado.getNombre());
 //       
 //    }
-    
-<<<<<<< HEAD
-=======
-    @Test
-    void guardarClienteValido() throws PersistenciaException {
 
-        // Preparamos un Cliente normal con todos sus campos obligatorios
-        Cliente cliente = new Cliente();
-        cliente.setNombre("Regina");
-        cliente.setApellidoPaterno("Jimenez");
-        cliente.setApellidoMaterno("Meneses");
-        cliente.setTelefono("6822491867");
-        cliente.setCorreoElectronico("regina@test.com");
-        
-        Cliente resultado = clienteDAO.guardar(cliente);
-        assertNotNull(resultado.getId(), "La base de datos debió generar un ID para el cliente");
-        assertEquals("Regina", resultado.getNombre());
-       
-    }
-
->>>>>>> 4f607930f10b8f0622778147fc827fabfa357e15
 }
