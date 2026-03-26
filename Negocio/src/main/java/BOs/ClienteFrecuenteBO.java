f366624866946e6abdbb8240c4b4d91e58786650
Laisak26 <isaacgerhardus@gmail.com>
2026-03-26 10:39:14 -0700

    Feat: Creacion de ClienteBO
    Creacion de la clase ClienteBO y pequenos cambios de coherencia en la interfaz ClienteFrecuenteBO

--- Negocio/src/main/java/BOs/ClienteFrecuenteBO.java
+++ Negocio/src/main/java/BOs/ClienteFrecuenteBO.java
@@ -1,38 +1,38 @@
-//package BOs;
-//
-//import dtos.ClienteFrecuenteDTO;
-//import interfaces.IClienteFrecuenteBO;
-//import java.util.List;
-//
-///**
-// *
-// * @author Isaac
-// */
-//public class ClienteFrecuenteBO implements IClienteFrecuenteBO{
-//
-//    @Override
-//    public ClienteFrecuenteDTO registrar(ClienteFrecuenteDTO clienteFrecuenteDTO) throws Exception {
-//        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-//    }
-//
-//    @Override
-//    public ClienteFrecuenteDTO actualizar(ClienteFrecuenteDTO clienteFrecuenteDTO) throws Exception {
-//        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-//    }
-//
-//    @Override
-//    public List<ClienteFrecuenteDTO> consultarTodos() throws Exception {
-//        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-//    }
-//
-//    @Override
-//    public ClienteFrecuenteDTO buscarPorId(Long idCliente) throws Exception {
-//        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-//    }
-//
-//    @Override
-//    public void eliminar(Long idCliente) throws Exception {
-//        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-//    }
-//    
-//}
+package BOs;
+
+import dtos.ClienteFrecuenteDTO;
+import interfaces.IClienteFrecuenteBO;
+import java.util.List;
+
+/**
+ *
+ * @author Isaac
+ */
+public class ClienteFrecuenteBO implements IClienteFrecuenteBO{
+
+    @Override
+    public ClienteFrecuenteDTO guardar(ClienteFrecuenteDTO clienteFrecuenteDTO) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public ClienteFrecuenteDTO editar(ClienteFrecuenteDTO clienteFrecuenteDTO) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<ClienteFrecuenteDTO> consultarTodos() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public ClienteFrecuenteDTO buscarPorId(Long idCliente) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void eliminar(Long idCliente) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
