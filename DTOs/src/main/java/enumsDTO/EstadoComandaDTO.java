954d3f284cd6cf6b74964e3f70eadccde43124ed
Laisak26 <isaacgerhardus@gmail.com>
2026-04-12 20:13:36 -0700

    feat y fix: Creacion del DTO de estadocomanda y estadomesa
    Tambien se implemento el cambio lso dtos en el dto de comanda y mesa

---
+++ DTOs/src/main/java/enumsDTO/EstadoComandaDTO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package enumsDTO;
+
+/**
+ * Representa el estado de una comanda en la capa DTO
+ * 
+ * @author regina, mariana e isaac
+ */
+public enum EstadoComandaDTO {
+    ABIERTA,
+    ENTREGADA,
+    CANCELADA
+}
